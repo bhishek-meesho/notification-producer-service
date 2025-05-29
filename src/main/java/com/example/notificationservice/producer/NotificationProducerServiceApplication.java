b0abd0abff44de7a61dbc9012dc40634b8f0cbbf
Bhishek Parmar <bhishek.parmar@meesho.com>
2025-05-29 16:16:09 +0530

    feat: Initial Spring Boot Notification Producer project setup

---
+++ src/main/java/com/example/notificationservice/producer/NotificationProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationservice.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProducerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationProducerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notificationservice/producer/NotificationProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationservice.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
